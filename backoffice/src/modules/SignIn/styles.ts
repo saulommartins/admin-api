10b3eaac25403c7e2abce74ed4cf3469d819326a
Saulo Mendes Martins <saulommartins@gmail.com>
2020-06-30 22:35:31 -0300

    Add new login layout material-ui

--- backoffice/src/modules/SignIn/styles.ts
+++ backoffice/src/modules/SignIn/styles.ts
@@ -1,61 +1,72 @@
 import styled, { css } from 'styled-components';
-import { TextField } from '@material-ui/core';
+import {
+  TextField,
+  Card,
+  CardActions,
+  Button
+} from '@material-ui/core';
 
-export const Container = styled.div`
+export const Container = styled.div`  
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  min-height: 100vh;
   align-items: center;
-  height: 100vh;
-  padding: 0 20px;
+  justify-content: center;
+  background: url(https://source.unsplash.com/random/1600x900);
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
 export const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: flex-end;
-  width: 100%;
-  max-width: 350px;
-  max-height: 270px;
-  height: 100%;
-  padding: 40px;
-  background-color: #fff;
-  border-radius: 10px;
-  box-sizing: border-box;
-  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
+`;
+
+export const SignInCard = styled(Card)`
+  min-width: 300px;
+`;
+
+export const SignInFields = styled.div`
+  padding: 1em 1em 1em 1em;
 `;
 
 const sharedStyles = css`
-  background-color: #eee;
   border-radius: 5px;
   border: 1px solid #ddd;
   margin: 10px 0 20px 0;
-  padding: 40px;
   box-sizing: border-box;
+  margin-top: 1em;
 `;
 
 export const UserName = styled(TextField)`
-  /* display: block; */
   width: 100%;
   ${sharedStyles}
 `;
 
 export const Password = styled(TextField)`
-  /* display: block; */
   width: 100%;
   ${sharedStyles}
 `;
 
-export const SubmitButton = styled.button`
-  /* display: block; */
+export const SignInCardActions = styled(CardActions)`
+  margin-top: 15px;
+  margin-bottom: 15px;
+  padding: 0;
+`;
+
+export const SubmitButton = styled(Button)`
   background-color: var(--senary);
   color: #fff;
   font-size: 0.9rem;
   border: 0;
   border-radius: 5px;
   height: 40px;
-  padding: 0 20px;
   cursor: pointer;
   box-sizing: border-box;
+`;
+
+export const Notification = styled.div`
+  text-align: right!important;
+  > span {
+    color: var(--notification);
+    font-size: 13px;
+  }
 `;
